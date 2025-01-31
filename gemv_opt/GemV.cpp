232c543fe4dbce2483627335b3158e5b238af5ea
Aba <abarajithan07@gmail.com>
2025-01-31 14:09:28 -0800

    i16,DX=DY=16,a48 Core:II=87, L=23

--- gemv_opt/GemV.cpp
+++ gemv_opt/GemV.cpp
@@ -7,18 +7,39 @@
 #include <stdlib.h>
 #include <type_traits>
 
-template <int DX, int DY>
-void GemV(adf::input_buffer<DTYPE>& __restrict in, 
-               adf::output_buffer<DTYPE>& __restrict out)
-{
-    aie::vector<DTYPE, DX> vx = aie::load_v<DX>((DTYPE*)in.data());
-    aie::accum<acc48, DY> acc (aie::zeros<acc48,DY>());
+// GemV: 44 clocks
+// Main: 42
+// II  : 87
+// Lat : 23
 
-    for (int i2=0; i2<DX/2; i2++) {
-        int id = i2*DY;
-        aie::vector<DTYPE,DY> buf_mat0 = aie::load_v<DY>((DTYPE*)matrix_0 + id);
-        aie::vector<DTYPE,DY> buf_mat1 = aie::load_v<DY>((DTYPE*)matrix_1 + id);
-        acc = mac16(acc,concat(buf_mat0, buf_mat1), 0,0x73727170, 0x77767574, 0x3120, vx, i2*2,0x0,0x0,1);
+
+template <int DX, int DY>
+void GemV(
+    adf::input_buffer <DTYPE>& __restrict in, 
+    adf::output_buffer<DTYPE>& __restrict out)
+{
+    aie::accum<acc48, DY> acc (aie::zeros<acc48,DY>());
+    aie::vector<DTYPE,DY> buf_mat0, buf_mat1;
+    aie::vector<DTYPE,DX> vx = aie::load_v<DX>((DTYPE*)in.data());
+
+    for (int i=0, id=0; i<DX; i+=2, id+=DY) {
+
+        buf_mat0 = aie::load_v<DY>((DTYPE*)matrix_0 + id);
+        buf_mat1 = aie::load_v<DY>((DTYPE*)matrix_1 + id);
+        
+        acc = mac16(
+            acc,
+            concat(buf_mat0, buf_mat1), 
+            0,
+            0x73727170, 
+            0x77767574, 
+            0x3120, 
+            vx, 
+            i,
+            0x0,
+            0x0,
+            1
+        );
     }
 
     aie::vector<DTYPE, DY> vy = acc.template to_vector<DTYPE>(0);
